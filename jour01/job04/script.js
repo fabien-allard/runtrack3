afd25611bfef5d50a0dc2f72bb15d171ff48b04d
fabien-allard <91756104+fabien-allard@users.noreply.github.com>
2022-03-22 16:28:37 +0100

    add file js & php

---
+++ jour01/job04/script.js
@@ -0,0 +1,13 @@
+function bisextile(annee){
+    
+    if (annee%4==0) {
+        console.log("true");
+        return true;
+    }
+    else {
+        console.log("false");
+        return false;
+    }
+}
+
+bisextile(2028);
