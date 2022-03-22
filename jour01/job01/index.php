2a7ba0ba4dff117ba87579791f6ba2c2ad356b88
fabien-allard <91756104+fabien-allard@users.noreply.github.com>
2022-03-22 16:26:07 +0100

    Add files via upload

--- jour01/job01/index.php
+++ jour01/job01/index.php
@@ -1,1 +1,16 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script>
+    let message = 'Hello Javascript ! ';
+    windows.alert(message);
+    </script>
+    <title>Runtack3-js-jour01</title>
+</head>
+<body>
+<a href="javascript:popupBasique('popup.html')">Hello Javascript!</a>
+</body>
+</html>
