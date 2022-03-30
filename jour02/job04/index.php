d4c5825db5408031252ff077d2c7509e7bab8e69
fabien-allard <91756104+fabien-allard@users.noreply.github.com>
2022-03-30 17:25:59 +0200

    add files php and js

--- jour02/job04/index.php
+++ jour02/job04/index.php
@@ -1,1 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> runctrack3 jour 02 Job 04</title>
+</head>
+<body>
+        <textarea id="textarea" onkeypress="return keylogger(event)"></textarea>
+        <script type="text/javascript" src="script.js"></script>
 
+</body>
+</html>
