7560eec3771f1d54ee406b4b8e463a50d1af5b35
fabien-allard <91756104+fabien-allard@users.noreply.github.com>
2022-03-30 17:24:55 +0200

    add file php and js

--- jour02/job03/index.php
+++ jour02/job03/index.php
@@ -1,1 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> runctrack3 jour 02 Job 03</title>
+</head>
+<body>
+    <p>Bienvenue sur la Runtrack3 job3</p>
+    <button id="button">Compteur</button>
+    <article id="compteur">0</article>
+    <script type="text/javascript" src="script.js"></script>
 
+</body>
+</html>
