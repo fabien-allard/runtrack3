18078edff74d66111ceff9bcdaa7e4c5f4613f87
fabien-allard <91756104+fabien-allard@users.noreply.github.com>
2022-03-30 17:23:53 +0200

    add file php and js

--- jour02/job02/index.php
+++ jour02/job02/index.php
@@ -1,1 +1,14 @@
-
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>runtrack3/jour02/job02</title>
+</head>
+<body>
+    <button id="button">ON/OFF</button>
+    <article id="citation"></article>
+    <script type="text/javascript" src="script.js"></script>
+</body>
+</html>
---
+++ jour02/job02/script.js
@@ -0,0 +1,16 @@
+var button = document.getElementById("button");
+var article = document.getElementById('citation');
+
+// Apparition, Disparition du texte 
+function showhide () {
+
+    document.getElementById("citation").innerHTML = "L'important n'est pas la chute, mais l'atterrissage.";
+
+    if (getComputedStyle(article).display != "none") {
+        article.style.display ="none";
+    } else  {
+        article.style.display = "block";
+    }
+}
+
+button.addEventListener('click', showhide);
